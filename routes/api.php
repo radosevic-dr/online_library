0c2f0f738736770fa9bfc0c189105a6207d7a1c1
radosevic-dr <radosevic.dejan@proton.me>
2024-07-25 17:20:47 +0200

    https://matijabojanic.atlassian.net/browse/OL-13 user roles defined and database strucure implemented

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/auth/register', [UserController::class, 'register']);
-Route::post('/auth/login', [UserController::class, 'login']);
