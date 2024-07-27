690b04d94b50e93bd9c972857f454c5a0008af60
radosevic-dr <radosevic.dejan@proton.me>
2024-07-27 19:13:12 +0200

    Updated login functionality

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,55 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     
     public function login(Request $request)
     {
-        try {
-            $validateLogin = Validator::make($request->all(),
-                [
-                    'email' => ['required', 'email'],
-                    'password' => ['required', 'min:8', 'max:32'],
-                ]
-            );
 
-            if ($validateLogin->fails()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Validation Error',
-                    'errors' => $validateLogin->errors(),
-                ], 401);
-            }
+        $validateLogin = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:32'],
+        ]);
 
-            $user = User::where('email', $request->email)->first();
-
-            if($user){
-                session_start();
-                $_SESSION['user_id'] = $user->id;
-                $_SESSION['username'] = $user->username;
-            
-                $apiToken = $user->createToken('API TOKEN')->plainTextToken;
-
-                return response()->json([
-                    'status' => 'success',
-                    'message' => 'Successfully Logged In',
-                    'token' => $apiToken
-                ], 200);
-            }
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => 'false',
-                'message' => $th->getMessage(),
-            ], 500);
+        if(auth()->attempt($validateLogin)){
+            $user = User::where('email', $validateLogin['email'])->first();
+            $token = $user->createToken('API TOKEN')->plainTextToken;
+            return $token;
         }
-
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [UserController::class, 'login']);
-Route::post('/auth/register', [UserController::class, 'register']);
+
+
