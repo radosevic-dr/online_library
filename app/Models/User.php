59f2bdca8a5f6308cbbd93e5c4d4876c5cb1d85c
radosevic-dr <radosevic.dejan@proton.me>
2024-07-28 14:17:44 +0200

    https://matijabojanic.atlassian.net/browse/OL-16 user creation (register) functionality ready

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,39 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;'
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Notifications\Notifiable;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
+    public const USER_TYPE_STUDENT  = 'student';
+    public const USER_TYPE_LIBRARIAN  = 'librarian';
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('profile_picture')
+            ->singleFile()
+            ->registerMediaConversions(function (Media $media) {
+                $this->addMediaConversion('thumb')
+                    ->width(100)
+                    ->height(100);
+            });
+    }
+
     protected $fillable = [
         'name',
         'email',
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login', [UserController::class, 'login']);
+Route::post('/auth/login', [UserController::class, 'login'])->name('api.login');
 
-Route::group([
-    'middleware' => ['auth:sanctum'],
-], function () {
-
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/auth/register', [UserController::class, 'register'])->middleware('tokenAuth');
     Route::post('/auth/logout', [UserController::class, 'logout']);
 });
