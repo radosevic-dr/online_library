d22c86e1eb4a694a1979d9843cb513e1ac8f1511
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-07-29 14:48:57 +0200

    Some optimization

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+
 
 class CategoryController extends Controller
 {
