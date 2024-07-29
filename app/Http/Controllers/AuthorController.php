b27e8cae4d173a96bdb425f48ab458ca3d584a09
Vojislav Vukotic <77846637+VoxMne@users.noreply.github.com>
2024-07-29 11:43:25 +0200

    subtask\OL-52 Added Spatie Library functionality

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthorController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'biography' => 'nullable|string',
+            'picture' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $data = $request->only(['first_name', 'last_name', 'biography']);
+        $author = Author::create($data);
+
+        if ($request->hasFile('picture')) {
+            $author->addMedia($request->file('picture'))->toMediaCollection('pictures');
+        }
+
+        return response()->json($author->load('media'), 201);
+    }
+}
