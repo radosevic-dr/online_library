dd46244faaeeba59aa0f6e3272ee7b7536534c79
Vojislav Vukotic <77846637+VoxMne@users.noreply.github.com>
2024-07-30 00:00:48 +0200

    subtask/OL-54 Author Edit

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
@@ -34,7 +35,7 @@ class AuthorController extends Controller
     {
         $author = Author::with('media')->find($id);
 
-        if (! $author) {
+        if (!$author) {
             return response()->json(['error' => 'Author not found'], 404);
         }
 
@@ -45,7 +46,7 @@ class AuthorController extends Controller
     {
         $author = Author::find($id);
 
-        if (! $author) {
+        if (!$author) {
             return response()->json(['error' => 'Author not found'], 404);
         }
 
@@ -57,5 +58,35 @@ class AuthorController extends Controller
         $picture = $mediaItems->first();
 
         return response()->download($picture->getPath(), $picture->file_name);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json(['error' => 'Author not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'biography' => 'nullable|string',
+            'picture' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $data = $request->only(['first_name', 'last_name', 'biography']);
+        $author->update($data);
+
+        if ($request->hasFile('picture')) {
+            $author->clearMediaCollection('pictures');
+            $author->addMedia($request->file('picture'))->toMediaCollection('pictures');
+        }
+
+        return response()->json($author->load('media'), 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/authors', [AuthorController::class, 'store']);
 Route::get('/authors/{id}', [AuthorController::class, 'show']);
 Route::get('/authors/{id}/picture', [AuthorController::class, 'getPicture']);
+Route::put('/authors/{id}', [AuthorController::class, 'update']);
