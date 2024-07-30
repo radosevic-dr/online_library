d46dcb71bf650804652e53e71b94fd735622068f
radosevic-dr <radosevic.dejan@proton.me>
2024-07-30 18:57:07 +0200

    Removed unused class and method

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'tokenAuth' => \App\Http\Middleware\TokenAuthMiddleware::class
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/auth/register', [UserController::class, 'register'])->middleware('tokenAuth');
+    Route::post('/auth/register', [UserController::class, 'register']);
     Route::post('/auth/logout', [UserController::class, 'logout']);
 });
