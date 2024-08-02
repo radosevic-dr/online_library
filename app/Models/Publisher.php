2979e3d9e840a244495e2d215e4adc653bee3234
radosevic-dr <radosevic.dejan@proton.me>
2024-08-02 17:22:13 +0200

    Publisher Model

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Book;
-use App\Models\Author;
+// use App\Models\Book;
+// use App\Models\Author;
 
 class Publisher extends Model
 {
@@ -15,13 +15,13 @@ class Publisher extends Model
         'name', 'address', 'website', 'email', 'phone', 'established_year',
     ];
 
-    public function books()
-    {
-        return $this->hasMany(Book::class);
-    }
+    // public function books()
+    // {
+    //     return $this->hasMany(Book::class);
+    // }
 
-    public function authors()
-    {
-        return $this->belongsToMany(Author::class);
-    }
+    // public function authors()
+    // {
+    //     return $this->belongsToMany(Author::class);
+    // }
 }
