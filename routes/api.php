b389a474c4b3c6bdef43aa15e7ceed1306f42ae7
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-08-03 13:10:50 +0200

    https://matijabojanic.atlassian.net/browse/OL-72 Added route and controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [UserController::class, 'login']);
@@ -9,3 +10,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/register', [UserController::class, 'register']);
     Route::post('/auth/logout', [UserController::class, 'logout']);
 });
+
+Route::apiResource('publisher', PublisherController::class);
