7298d3fc5cc8b5715a0d50d538777e573c5ee8f9
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-08-03 13:36:47 +0200

    https://matijabojanic.atlassian.net/browse/OL-72 Optimization

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
-Route::
-post('/auth/login', [UserController::class, 'login']);
+Route::post('/auth/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/register', [UserController::class, 'register']);
