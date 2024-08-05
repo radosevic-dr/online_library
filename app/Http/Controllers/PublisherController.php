8c2b78c3642d2a22c674897d07afa5abb8494cfa
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-08-05 17:53:50 +0200

    https://matijabojanic.atlassian.net/browse/OL-73/Added view-all

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -5,23 +5,46 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PublisherResource;
 use App\Models\Publisher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class PublisherController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Publisher $publisher, Request $request)
+    public function index(Request $request)
     {
-//        $perPageOptions = [20, 50, 100];
-//
-//        $perPage = $request->input('per_page', 20);
-//
-//        if (!in_array($perPage, $perPageOptions)) {
-//            $perPage = 20;
-//        }
-//
-//        return PublisherResource::collection(Publisher::paginate($perPage));
+        $perPageOptions = [20, 50, 100];
+
+        $validator = Validator::make($request->all(), [
+            'per_page' => [
+                'nullable',
+                'integer',
+                Rule::in($perPageOptions)
+            ],
+            'search' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Invalid parameters',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $searchValue = $request->input('search', '');
+        $perPage = $request->input('per_page', 20);
+
+        $query = Publisher::query();
+
+        if (!empty($searchValue)) {
+            $query->where('name', 'LIKE', "%{$searchValue}%");
+        }
+
+        $publishers = $query->paginate($perPage);
+
+        return PublisherResource::collection($publishers);
     }
 
     /**
