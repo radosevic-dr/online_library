73feeea4c711a2133ae468b2a6c9f897e7efc654
radosevic-dr <radosevic.dejan@proton.me>
2024-08-09 15:25:23 +0200

    https://matijabojanic.atlassian.net/browse/OL-69

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ Route::post('/auth/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/auth/register', [UserController::class, 'register']);
-    Route::post('/auth/logout', [UserController::class, 'logout']);
+    Route::post('/auth/logout', [UserController::class, 'logout']); 
 });
